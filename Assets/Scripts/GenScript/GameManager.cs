887764a73a27ab92ff5d61459ff0417a4d03325e
Decisy Gaëtan <gaetan.decisy6@hotmail.com>
2025-01-07 11:32:30 +0100

    Added more logic

--- Assets/Scripts/GenScript/GameManager.cs
+++ Assets/Scripts/GenScript/GameManager.cs
@@ -4,12 +4,14 @@ public class GameManager : MonoBehaviour
 {
     [Header("General Settings")] 
     [SerializeField] private int _playerNumber;
+    private int _bullet;
     private int _randomNumber;
     private int _roundCount;
 
     private void Start()
     {
-        _randomNumber = Random.Range(1, _playerNumber);
+        _bullet = _playerNumber;
+        _randomNumber = Random.Range(1, _bullet);
         _roundCount = 0;
     }
 
@@ -20,8 +22,9 @@ public class GameManager : MonoBehaviour
         if (_roundCount == _randomNumber)
         {
             Debug.Log("Dead, gameover, tu pues bouuh");
-            
-            _randomNumber = Random.Range(1, _playerNumber);
+
+            _bullet--;
+            _randomNumber = Random.Range(1, _bullet);
             _roundCount = 0;
         }
         else
