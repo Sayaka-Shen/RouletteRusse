0d0f9f9d2331ff2cd9aae84f1a6c1fdbdd556637
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2025-01-07 11:06:43 +0100

    Update Menu

---
+++ Assets/Script/ChoosePlayer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro;
+
+
+
+public class ChoosePlayer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _nbrPlayer;
+    private int _nbrPlayerIndex = 2;
+
+    private void Start()
+    {
+        UpdateUI();
+    }
+
+    public void AddNbrPlayer()
+    {
+        _nbrPlayerIndex += 1;
+        UpdateUI();
+    }
+
+    public void RemoveNbrPlayer()
+    {
+        _nbrPlayerIndex -= 1;
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        _nbrPlayer.text = _nbrPlayerIndex.ToString();
+    }
+
+}
