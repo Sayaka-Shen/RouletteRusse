7daa1bb0f9faad289ac35a49372688fe4e05a642
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2025-01-10 09:40:29 +0100

    Add hotreload + update chooseplayer

--- Assets/Script/ChoosePlayer.cs
+++ Assets/Script/ChoosePlayer.cs
@@ -6,11 +6,21 @@ using TMPro;
 public class ChoosePlayer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _nbrPlayer;
+    [SerializeField] private TextMeshProUGUI _roundPlayer;
     private int _nbrPlayerIndex = 2;
+    private int _roundIndex = 1;
 
     private void Start()
     {
         UpdateUI();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            NextRound();
+        }
     }
 
     public void AddNbrPlayer()
@@ -30,9 +40,10 @@ public class ChoosePlayer : MonoBehaviour
         _nbrPlayer.text = _nbrPlayerIndex.ToString();
     }
 
-    public void NameUI()
+    public void NextRound()
     {
-
+        _roundIndex++;
+        _roundPlayer.text = _roundIndex.ToString();
     }
 
 }
