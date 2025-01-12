3228bc069bdd91281c951a3c0591f5c689982d37
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2025-01-12 11:44:25 +0100

    add revolver, animation, sprite scroller for reload

--- Assets/Scripts/GenScript/GameManager.cs
+++ Assets/Scripts/GenScript/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,30 +11,42 @@ public class GameManager : MonoBehaviour
         EndRound
     }
 
-    [Header("General Settings")] 
+    [Header("General Settings")]
     [SerializeField] private int _playerNumber;
+    [SerializeField] private GameInfo _gameInfo;
+    [SerializeField] private TextMeshProUGUI _playerInfo;
     private int _playerCount;
     private int _deathChance;
     private int _randomNumber;
+    private int _roundCount = 0;
+
+    [Header("Animation")]
+    [SerializeField] private Animator _gunAnimator;
+    [SerializeField] private SpriteScroller _reloadAnim;
 
     private void Start()
     {
+        _playerNumber = _gameInfo.Players.Count;
         _playerCount = _playerNumber;
         _deathChance = 6;
-        
+
         PreRound();
     }
 
     private void PreRound()
     {
+        _roundCount++;
         Debug.Log("Le round commence !");
         MusicManager.Instance.PlayMusic("SlowHeartBeat");
         SoundManager.Instance.PlaySound2D("ReloadSound");
+        _reloadAnim.StartScroll();
+        DifferentUI();
     }
 
     public void Shoot()
     {
         _randomNumber = Random.Range(1, _deathChance);
+        _gunAnimator.SetTrigger("Shoot");
 
         // Mort le bro
         if (_randomNumber == 1)
@@ -41,10 +54,12 @@ public class GameManager : MonoBehaviour
             SoundManager.Instance.PlaySound2D("ShotSound");
             SoundManager.Instance.PlaySound2D("FallBodySound");
             SoundManager.Instance.PlaySound2D("ReloadSound");
+            _reloadAnim.StartScroll();
+
 
             _playerCount--;
             _deathChance = 6;
-            
+
             PostRound();
         }
         else // Vivant le type
@@ -52,7 +67,7 @@ public class GameManager : MonoBehaviour
             _deathChance--;
             SoundManager.Instance.PlaySound2D("EmptyShotSound");
             Debug.Log("Next Player");
-            
+
             if (_deathChance <= 4)
             {
                 MusicManager.Instance.PlayMusic("FastHeartBeat");
@@ -64,14 +79,34 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log($"Le player {_playerNumber - _playerCount} est mort! ");
 
-        if(_playerCount == 1)
+        if (_playerCount == 1)
         {
+            _roundCount = 0;
             Debug.Log("Fin du round tout le monde est mort sauf toi, rappuyer sur le bouton pour rejouer");
         }
         else
         {
-            PreRound();    
+            PreRound();
         }
-        
+
+    }
+
+    private void DifferentUI()
+    {
+        int index;
+        if (_roundCount % 2 == 1) // Si _roundCount est impair
+        {
+            index = _deathChance;
+        }
+        else
+        {
+            index = 7 - _deathChance;
+        }
+
+        if (index >= 1 && index <= _gameInfo.name.Length)
+        {
+            _playerInfo.text = _gameInfo.name[index].ToString();
+        }
     }
 }
+
---
+++ Assets/Scripts/Utility/SpriteScroller.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class SpriteScroller : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer; 
+    public float scrollSpeed = 1f;
+    public float scrollDuration = 2f; 
+
+    private Material material;
+    private float elapsedTime = 0f; 
+    private bool isScrolling = false;
+
+    void Start()
+    {
+        material = spriteRenderer.material;
+    }
+
+    public void StartScroll()
+    {
+        if (!isScrolling)
+        {
+            elapsedTime = 0f;
+            isScrolling = true;
+        }
+    }
+
+    void Update()
+    {
+        if (isScrolling)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime < scrollDuration)
+            {
+                float offset = elapsedTime * scrollSpeed;
+
+                material.mainTextureOffset = new Vector2(offset, 0f);
+            }
+            else
+            {
+                isScrolling = false;
+                material.mainTextureOffset = Vector2.zero;
+            }
+        }
+    }
+}
