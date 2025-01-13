60f780a73bcf7966a45494d71aebc0793f615787
LeLoader <leloaderpro@gmail.com>
2025-01-13 15:41:09 +0100

    Up

---
+++ Assets/DeathChanceOverlay.cs
@@ -0,0 +1,38 @@
+using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class DeathChanceOverlay : MonoBehaviour
+{
+    [SerializeField] GameObject WrapperDown;
+    [SerializeField] GameObject WrapperUp;
+    [SerializeField] GameObject WrapperLeft;
+    [SerializeField] GameObject WrapperRight;
+
+    [SerializeField] float duration = 1f;
+
+    Sequence sequence;
+
+
+    private void Awake()
+    {
+        //GameManager.OnDeathChanceChange += OnDeathChanceChange;
+        NewTween();
+    }
+
+    void OnDeathChanceChange(int newDC)
+    {
+        sequence.Kill();
+        duration = 1 / (6 / newDC);
+        NewTween();
+    }
+
+    void NewTween()
+    {
+        sequence = DOTween.Sequence();
+        sequence.Append(WrapperDown.transform.DOScaleY(1.2f, duration)).Join(WrapperUp.transform.DOScaleY(1.2f, duration)).Join(WrapperLeft.transform.DOScaleY(1.2f, duration)).Join(WrapperRight.transform.DOScaleY(1.2f, duration))
+                .Append(WrapperDown.transform.DOScaleY(1, duration)).Join(WrapperUp.transform.DOScaleY(1, duration)).Join(WrapperLeft.transform.DOScaleY(1, duration)).Join(WrapperRight.transform.DOScaleY(1, duration))
+                .SetEase(Ease.InOutSine).SetLoops(-1);
+        sequence.Play();
+    }
+}
